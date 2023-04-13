c7116b60dd616512dd2ff4802c973adf2684a523
Hieu Mula <hieu@mula-x.com>
2023-04-13 17:38:16 +0700

    update image path

--- src/layout/Logo.vue
+++ src/layout/Logo.vue
@@ -5,7 +5,7 @@
       class="hidden md:block cursor-pointer"
       height="100"
       width="100"
-      src="src/assets/images/logo.png"
+      src="/src/assets/images/logo.png"
     />
   </RouterLink>
 </template>
